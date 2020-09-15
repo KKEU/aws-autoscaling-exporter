863ffa7acf51b690d70f1e3920e575bd3c7e951a
Phillip Stockmann <phillip.stockmann@kaiserkraft-europa.de>
2020-09-15 15:22:39 +0200

    feat: remove querying of banzaicloud spot-recommender

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## AWS Autoscaling exporter
 
-Prometheus exporter for AWS auto scaling groups, part of the [Hollowtrees](https://github.com/banzaicloud/hollowtrees) project. Provides auto scaling group level metrics similar to CloudWatch metrics and instance level metrics for spot instances in the auto scaling group. For group level metrics the exporter is polling the AWS APIs for auto scaling groups. For instance level metrics it queries the Banzai Cloud spot instance [recommender API](https://github.com/banzaicloud/spot-recommender) to report cost and stability related metrics for spot instances.
+Prometheus exporter for AWS auto scaling groups, forked from https://github.com/banzaicloud/aws-autoscaling-exporter project. In contrast to the parent project this fork has been reduced to not access their spot-recommender anymore.
+
+Provides auto scaling group level metrics similar to CloudWatch metrics. For group level metrics the exporter is polling the AWS APIs for auto scaling groups.
 
 ### Quick start
 
@@ -20,8 +22,6 @@ Usage of ./aws-autoscaling-exporter:
         log level (default "info")
   -metrics-path string
         path to metrics endpoint (default "/metrics")
-  -recommender-url string
-        URL of the spot instance recommender (default "http://localhost:9090")
   -region string
         AWS region that the exporter should query (default "eu-west-1")
 ```
@@ -56,27 +56,4 @@ aws_autoscaling_standby_instances_total{asg_name="marci-test",region="eu-west-1"
 # HELP aws_autoscaling_terminating_instances_total Total number of terminating instances in the auto scaling group
 # TYPE aws_autoscaling_terminating_instances_total gauge
 aws_autoscaling_terminating_instances_total{asg_name="marci-test",region="eu-west-1"} 0
-# HELP aws_instance_cost_score Current cost score of spot instance reported by the spot recommender
-# TYPE aws_instance_cost_score gauge
-aws_instance_cost_score{asg_name="marci-test",availability_zone="eu-west-1a",instance_id="i-061ae0a2960e194be",instance_type="m5.xlarge",region="eu-west-1"} 0.787585
-# HELP aws_instance_current_price Current price of spot instance reported by the spot recommender.
-# TYPE aws_instance_current_price gauge
-aws_instance_current_price{asg_name="marci-test",availability_zone="eu-west-1a",instance_id="i-061ae0a2960e194be",instance_type="m5.xlarge",region="eu-west-1"} 0.0824
-# HELP aws_instance_on_demand_price Current on demand price of spot instance reported by the spot recommender
-# TYPE aws_instance_on_demand_price gauge
-aws_instance_on_demand_price{asg_name="marci-test",availability_zone="eu-west-1a",instance_id="i-061ae0a2960e194be",instance_type="m5.xlarge",region="eu-west-1"} 0.214
-# HELP aws_instance_optimal_bid_price Optimal spot bid price of instance reported by the spot recommender
-# TYPE aws_instance_optimal_bid_price gauge
-aws_instance_optimal_bid_price{asg_name="marci-test",availability_zone="eu-west-1a",instance_id="i-061ae0a2960e194be",instance_type="m5.xlarge",region="eu-west-1"} 0.214
-# HELP aws_instance_spot_bid_price Spot bid price used to request the spot instance
-# TYPE aws_instance_spot_bid_price gauge
-aws_instance_spot_bid_price{asg_name="marci-test",availability_zone="eu-west-1a",instance_id="i-061ae0a2960e194be",instance_type="m5.xlarge",region="eu-west-1"} 0.214
-# HELP aws_instance_stability_score Current stability score of spot instance reported by the spot recommender
-# TYPE aws_instance_stability_score gauge
-aws_instance_stability_score{asg_name="marci-test",availability_zone="eu-west-1a",instance_id="i-061ae0a2960e194be",instance_type="m5.xlarge",region="eu-west-1"} 0
 ```
-
-### Default Hollowtrees node exporters associated to alerts:
-
-* AWS spot instance termination [collector](https://github.com/banzaicloud/spot-termination-collector)
-* AWS autoscaling group [exporter](https://github.com/banzaicloud/aws-autoscaling-exporter)
--- exporter/exporter.go
+++ exporter/exporter.go
@@ -1,11 +1,7 @@
 package exporter
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"net/http"
-	"strconv"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -13,7 +9,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
-	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/prometheus/client_golang/prometheus"
 	log "github.com/sirupsen/logrus"
 )
@@ -22,7 +17,6 @@ import (
 type Exporter struct {
 	session         *session.Session
 	groups          []string
-	recommenderUrl  string
 	duration        prometheus.Gauge
 	scrapeErrors    prometheus.Gauge
 	totalScrapes    prometheus.Counter
@@ -49,18 +43,6 @@ type InstanceScrapeResult struct {
 	InstanceType     string
 }
 
-type Recommendation map[string][]InstanceTypeRecommendation
-
-type InstanceTypeRecommendation struct {
-	InstanceTypeName   string `json:"InstanceTypeName"`
-	CurrentPrice       string `json:"CurrentPrice"`
-	AvgPriceFor24Hours string `json:"AvgPriceFor24Hours"`
-	OnDemandPrice      string `json:"OnDemandPrice"`
-	SuggestedBidPrice  string `json:"SuggestedBidPrice"`
-	CostScore          string `json:"CostScore"`
-	StabilityScore     string `json:"StabilityScore"`
-}
-
 type instanceScrapeError struct {
 	count uint64
 }
@@ -70,7 +52,7 @@ func (e *instanceScrapeError) Error() string {
 }
 
 // NewExporter returns a new exporter of AWS Autoscaling group metrics.
-func NewExporter(region string, groups []string, recommenderUrl string) (*Exporter, error) {
+func NewExporter(region string, groups []string) (*Exporter, error) {
 
 	session, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
@@ -83,7 +65,6 @@ func NewExporter(region string, groups []string, recommenderUrl string) (*Export
 	e := Exporter{
 		session:        session,
 		groups:         groups,
-		recommenderUrl: recommenderUrl,
 		duration: prometheus.NewGauge(prometheus.GaugeOpts{
 			Namespace: "aws_autoscaling",
 			Name:      "scrape_duration_seconds",
@@ -139,37 +120,6 @@ func (e *Exporter) initGauges() {
 		Help:      "Total number of instances in the auto scaling group",
 	}, []string{"asg_name", "region"})
 
-	e.instanceMetrics = map[string]*prometheus.GaugeVec{}
-	e.instanceMetrics["spot_bid_price"] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "aws_instance",
-		Name:      "spot_bid_price",
-		Help:      "Spot bid price used to request the spot instance",
-	}, []string{"asg_name", "region", "instance_id", "instance_type", "availability_zone"})
-	e.instanceMetrics["cost_score"] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "aws_instance",
-		Name:      "cost_score",
-		Help:      "Current cost score of spot instance reported by the spot recommender",
-	}, []string{"asg_name", "region", "instance_id", "instance_type", "availability_zone"})
-	e.instanceMetrics["stability_score"] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "aws_instance",
-		Name:      "stability_score",
-		Help:      "Current stability score of spot instance reported by the spot recommender",
-	}, []string{"asg_name", "region", "instance_id", "instance_type", "availability_zone"})
-	e.instanceMetrics["on_demand_price"] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "aws_instance",
-		Name:      "on_demand_price",
-		Help:      "Current on demand price of spot instance reported by the spot recommender",
-	}, []string{"asg_name", "region", "instance_id", "instance_type", "availability_zone"})
-	e.instanceMetrics["optimal_bid_price"] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "aws_instance",
-		Name:      "optimal_bid_price",
-		Help:      "Optimal spot bid price of instance reported by the spot recommender",
-	}, []string{"asg_name", "region", "instance_id", "instance_type", "availability_zone"})
-	e.instanceMetrics["current_price"] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "aws_instance",
-		Name:      "current_price",
-		Help:      "Current price of spot instance reported by the spot recommender.",
-	}, []string{"asg_name", "region", "instance_id", "instance_type", "availability_zone"})
 }
 
 // Describe outputs metric descriptions.
@@ -189,23 +139,18 @@ func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 
 	groupScrapes := make(chan GroupScrapeResult)
-	instanceScrapes := make(chan InstanceScrapeResult)
 
 	e.Lock()
 	defer e.Unlock()
 
 	e.initGauges()
-	go e.scrape(groupScrapes, instanceScrapes)
+	go e.scrape(groupScrapes)
 
 	var wg sync.WaitGroup
 	wg.Add(2)
 	go func() {
 		defer wg.Done()
 		e.setGroupMetrics(groupScrapes)
-	}()
-	go func() {
-		defer wg.Done()
-		e.setInstanceMetrics(instanceScrapes)
 	}()
 	wg.Wait()
 
@@ -221,10 +166,9 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 	}
 }
 
-func (e *Exporter) scrape(groupScrapes chan<- GroupScrapeResult, instanceScrapes chan<- InstanceScrapeResult) {
+func (e *Exporter) scrape(groupScrapes chan<- GroupScrapeResult) {
 
 	defer close(groupScrapes)
-	defer close(instanceScrapes)
 	now := time.Now().UnixNano()
 	e.totalScrapes.Inc()
 
@@ -241,7 +185,6 @@ func (e *Exporter) scrape(groupScrapes chan<- GroupScrapeResult, instanceScrapes
 			wg.Add(1)
 			go func(asg *autoscaling.Group) {
 				defer wg.Done()
-				var recommendation *Recommendation
 				describeLcOutput, err := asgSvc.DescribeLaunchConfigurations(&autoscaling.DescribeLaunchConfigurationsInput{
 					LaunchConfigurationNames: []*string{asg.LaunchConfigurationName},
 				})
@@ -252,13 +195,12 @@ func (e *Exporter) scrape(groupScrapes chan<- GroupScrapeResult, instanceScrapes
 					log.WithField("autoScalingGroup", *asg.AutoScalingGroupName).Error("Failed to fetch launch configuration for auto scaling group, recommendation related metrics will not be reported.")
 					atomic.AddUint64(&errorCount, 1)
 				} else {
-					recommendation, err = e.getRecommendations(*describeLcOutput.LaunchConfigurations[0].InstanceType)
 					if err != nil {
 						log.WithField("autoScalingGroup", *asg.AutoScalingGroupName).WithError(err).Error("Failed to get recommendations, recommendation related metrics will not be reported.")
 						atomic.AddUint64(&errorCount, 1)
 					}
 				}
-				if err := e.scrapeAsg(groupScrapes, instanceScrapes, asg, recommendation); err != nil {
+				if err := e.scrapeAsg(groupScrapes, asg); err != nil {
 					log.WithField("autoScalingGroup", *asg.AutoScalingGroupName).Error(err)
 					if e, ok := err.(*instanceScrapeError); ok {
 						atomic.AddUint64(&errorCount, e.count)
@@ -298,56 +240,10 @@ func (e *Exporter) setGroupMetrics(scrapes <-chan GroupScrapeResult) {
 	}
 }
 
-func (e *Exporter) setInstanceMetrics(scrapes <-chan InstanceScrapeResult) {
-	log.Debug("set instance metrics")
-	for scr := range scrapes {
-		name := scr.Name
-		if _, ok := e.instanceMetrics[name]; !ok {
-			e.metricsMtx.Lock()
-			e.instanceMetrics[name] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-				Namespace: "aws_instance",
-				Name:      name,
-			}, []string{"asg_name", "region", "instance_id", "instance_type", "availability_zone"})
-			e.metricsMtx.Unlock()
-		}
-		var labels prometheus.Labels = map[string]string{
-			"asg_name":          scr.AutoScalingGroup,
-			"region":            scr.Region,
-			"instance_id":       scr.InstanceId,
-			"instance_type":     scr.InstanceType,
-			"availability_zone": scr.AvailabilityZone,
-		}
-		e.instanceMetrics[name].With(labels).Set(float64(scr.Value))
-	}
-}
-
-func (e *Exporter) getRecommendations(instanceType string) (*Recommendation, error) {
-	if instanceType == "" {
-		return nil, errors.New("no instance type specified for recommendation")
-	}
-	fullRecommendationUrl := fmt.Sprintf("%s/api/v1/recommender/%s?baseInstanceType=%s", e.recommenderUrl, *e.session.Config.Region, instanceType)
-	res, err := http.Get(fullRecommendationUrl)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	var recommendation *Recommendation
-	if res.StatusCode != http.StatusOK {
-		return nil, errors.New(fmt.Sprintf("Couldn't get recommendations: GET '%s': response code = '%s', expecting '200 OK'", fullRecommendationUrl, res.Status))
-	}
-	recommendation = new(Recommendation)
-	err = json.NewDecoder(res.Body).Decode(recommendation)
-	if err != nil {
-		return nil, err
-	}
-	return recommendation, nil
-}
-
-func (e *Exporter) scrapeAsg(groupScrapes chan<- GroupScrapeResult, instanceScrapes chan<- InstanceScrapeResult, asg *autoscaling.Group, recommendation *Recommendation) error {
+func (e *Exporter) scrapeAsg(groupScrapes chan<- GroupScrapeResult, asg *autoscaling.Group) error {
 	log.WithField("autoScalingGroup", *asg.AutoScalingGroupName).Debug("getting metrics from the auto scaling group")
 
-	var pendingInstances, inServiceInstances, standbyInstances, terminatingInstances, spotInstances int
+	var pendingInstances, inServiceInstances, standbyInstances, terminatingInstances int
 	var instanceIds []*string
 
 	if len(asg.Instances) > 0 {
@@ -401,162 +297,13 @@ func (e *Exporter) scrapeAsg(groupScrapes chan<- GroupScrapeResult, instanceScra
 	if len(instanceIds) > 0 {
 		var err error
 		log.WithField("autoScalingGroup", *asg.AutoScalingGroupName).Debug("getting metrics from the instances in the autoscaling group")
-		spotInstances, err = e.scrapeInstances(instanceScrapes, *asg.AutoScalingGroupName, instanceIds, recommendation)
 		if err != nil {
-			if e, ok := err.(*instanceScrapeError); ok {
-				countError = e
-			} else {
-				return err
-			}
+			return err
 		}
-	}
-
-	groupScrapes <- GroupScrapeResult{
-		Name:             "spot_instances_total",
-		Value:            float64(spotInstances),
-		AutoScalingGroup: *asg.AutoScalingGroupName,
-		Region:           *e.session.Config.Region,
 	}
 
 	if countError != nil {
 		return countError
 	}
 	return nil
-}
-
-func (e *Exporter) scrapeInstances(scrapes chan<- InstanceScrapeResult, asgName string, instanceIds []*string, recommendation *Recommendation) (int, error) {
-	var errorCount uint64
-	ec2Svc := ec2.New(e.session, aws.NewConfig())
-	var spotRequests []*string
-
-	err := ec2Svc.DescribeInstancesPages(&ec2.DescribeInstancesInput{
-		InstanceIds: instanceIds,
-	}, func(output *ec2.DescribeInstancesOutput, lastPage bool) bool {
-		for _, reservation := range output.Reservations {
-			for _, instance := range reservation.Instances {
-				if instance.SpotInstanceRequestId != nil {
-					spotRequests = append(spotRequests, instance.SpotInstanceRequestId)
-				}
-			}
-		}
-		return true
-	})
-	if err != nil {
-		return 0, err
-	}
-
-	if len(spotRequests) > 0 {
-		describeSpotInstanceRequestsOutput, err := ec2Svc.DescribeSpotInstanceRequests(&ec2.DescribeSpotInstanceRequestsInput{
-			SpotInstanceRequestIds: spotRequests,
-		})
-		if err != nil {
-			return 0, err
-		}
-
-		for _, spotRequest := range describeSpotInstanceRequestsOutput.SpotInstanceRequests {
-			spotBidPrice, err := strconv.ParseFloat(*spotRequest.SpotPrice, 64)
-			if err != nil {
-				log.WithField("autoScalingGroup", asgName).Error(err)
-				errorCount++
-			} else {
-				scrapes <- InstanceScrapeResult{
-					Name:             "spot_bid_price",
-					Value:            spotBidPrice,
-					AutoScalingGroup: asgName,
-					Region:           *e.session.Config.Region,
-					InstanceId:       *spotRequest.InstanceId,
-					AvailabilityZone: *spotRequest.LaunchedAvailabilityZone,
-					InstanceType:     *spotRequest.LaunchSpecification.InstanceType,
-				}
-			}
-
-			if recommendation != nil {
-				for _, instanceTypeRecommendation := range (*recommendation)[*spotRequest.LaunchedAvailabilityZone] {
-					if instanceTypeRecommendation.InstanceTypeName == *spotRequest.LaunchSpecification.InstanceType {
-						costScore, err := strconv.ParseFloat(instanceTypeRecommendation.CostScore, 64)
-						if err != nil {
-							log.WithField("autoScalingGroup", asgName).Error(err)
-							errorCount++
-						} else {
-							scrapes <- InstanceScrapeResult{
-								Name:             "cost_score",
-								Value:            costScore,
-								AutoScalingGroup: asgName,
-								Region:           *e.session.Config.Region,
-								InstanceId:       *spotRequest.InstanceId,
-								AvailabilityZone: *spotRequest.LaunchedAvailabilityZone,
-								InstanceType:     *spotRequest.LaunchSpecification.InstanceType,
-							}
-						}
-						stabilityScore, err := strconv.ParseFloat(instanceTypeRecommendation.StabilityScore, 64)
-						if err != nil {
-							log.WithField("autoScalingGroup", asgName).Error(err)
-							errorCount++
-						} else {
-							scrapes <- InstanceScrapeResult{
-								Name:             "stability_score",
-								Value:            stabilityScore,
-								AutoScalingGroup: asgName,
-								Region:           *e.session.Config.Region,
-								InstanceId:       *spotRequest.InstanceId,
-								AvailabilityZone: *spotRequest.LaunchedAvailabilityZone,
-								InstanceType:     *spotRequest.LaunchSpecification.InstanceType,
-							}
-						}
-						currentPrice, err := strconv.ParseFloat(instanceTypeRecommendation.CurrentPrice, 64)
-						if err != nil {
-							log.WithField("autoScalingGroup", asgName).Error(err)
-							errorCount++
-						} else {
-							scrapes <- InstanceScrapeResult{
-								Name:             "current_price",
-								Value:            currentPrice,
-								AutoScalingGroup: asgName,
-								Region:           *e.session.Config.Region,
-								InstanceId:       *spotRequest.InstanceId,
-								AvailabilityZone: *spotRequest.LaunchedAvailabilityZone,
-								InstanceType:     *spotRequest.LaunchSpecification.InstanceType,
-							}
-						}
-						onDemandPrice, err := strconv.ParseFloat(instanceTypeRecommendation.OnDemandPrice, 64)
-						if err != nil {
-							log.WithField("autoScalingGroup", asgName).Error(err)
-							errorCount++
-						} else {
-							scrapes <- InstanceScrapeResult{
-								Name:             "on_demand_price",
-								Value:            onDemandPrice,
-								AutoScalingGroup: asgName,
-								Region:           *e.session.Config.Region,
-								InstanceId:       *spotRequest.InstanceId,
-								AvailabilityZone: *spotRequest.LaunchedAvailabilityZone,
-								InstanceType:     *spotRequest.LaunchSpecification.InstanceType,
-							}
-						}
-						optimalBidPrice, err := strconv.ParseFloat(instanceTypeRecommendation.SuggestedBidPrice, 64)
-						if err != nil {
-							log.WithField("autoScalingGroup", asgName).Error(err)
-							errorCount++
-						} else {
-							scrapes <- InstanceScrapeResult{
-								Name:             "optimal_bid_price",
-								Value:            optimalBidPrice,
-								AutoScalingGroup: asgName,
-								Region:           *e.session.Config.Region,
-								InstanceId:       *spotRequest.InstanceId,
-								AvailabilityZone: *spotRequest.LaunchedAvailabilityZone,
-								InstanceType:     *spotRequest.LaunchSpecification.InstanceType,
-							}
-						}
-						break
-					}
-				}
-			}
-		}
-	}
-
-	if errorCount > 0 {
-		return len(spotRequests), &instanceScrapeError{errorCount}
-	}
-	return len(spotRequests), nil
 }
